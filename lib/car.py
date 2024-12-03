50f357d6159aef1dc442cfd5d8644a0836ef34fb
Joy-mwende63 <mwendejoy47@gmail.com>
2024-12-03 10:38:49 +0300

    complete lab

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
+# Import the Vehicle class to create a Car class that inherits from it
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    # Override the go() method to return a custom string for the Car class
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
